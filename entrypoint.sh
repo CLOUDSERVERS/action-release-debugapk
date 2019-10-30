84db8a5736fc3c70869ef69570d0bb437b7f83e5
Taylor Allen <28820818+allenbyus@users.noreply.github.com>
2019-10-31 07:57:44 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 hub checkout master
-hub release create -a ./${APP_FOLDER}/build/outputs/apk/debug/app-debug.apk -m "${RELEASE_TITLE}" $(date +%Y%m%d%H%M%S) 
+SEEK=`date '%Y%m%d%H%M%S'`
+VERSION=`echo "obase=64;${SEEK}"|bc`
+hub release create -a ./${APP_FOLDER}/build/outputs/apk/debug/app-debug.apk -m "${RELEASE_TITLE}${VERSION}" $(date +%Y%m%d%H%M%S) 
