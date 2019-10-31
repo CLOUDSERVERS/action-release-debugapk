cd23379ef547cc5823200cdaaf3985486f2f4753
Taylor Allen <28820818+allenbyus@users.noreply.github.com>
2019-10-31 12:00:04 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 hub checkout master
 SEEK=`date '+%Y%m%d%H%M%S'`
 VERSION=`echo "obase=16;${SEEK}"|bc`
-hub release create -a ./${APP_FOLDER}/build/outputs/apk/debug/app-debug.apk -m "${RELEASE_TITLE}${VERSION}" "Automatic generated" 
+hub release create -a ./${APP_FOLDER}/build/outputs/apk/debug/app-debug.apk -m "${RELEASE_TITLE_HEAD}${VERSION}${RELEASE_TITLE_FOOT}" "Automatic generated" 
