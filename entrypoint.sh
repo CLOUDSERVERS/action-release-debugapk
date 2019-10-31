ee369786a71d74321917b4b041e543ddad467b17
Taylor Allen <28820818+allenbyus@users.noreply.github.com>
2019-10-31 17:33:55 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ SEEK=`date '+%Y%m%d%H%M%S'`
 VERSION_NAME=`echo "obase=16;${SEEK}"|bc|tr '[:upper:]' '[:lower:]'`
 VERSION_CODE=`date '+%Y%U%w'`
 cat ./app/build.gradle
-sed -i 's/versionCode.*/versionCode ${VERSION_CODE}/1' ./app/build.gradle
+sed -i "s/versionCode.*/versionCode ${VERSION_CODE}/1" ./app/build.gradle
 cat ./app/build.gradle
-sed -i 's/versionName.*/versionName "${VERSION_NAME}-alpha"/1' ./app/build.gradle
+sed -i "s/versionName.*/versionName \"${VERSION_NAME}-alpha\"/1" ./app/build.gradle
 cat ./app/build.gradle
 hub release create -a ./${APP_FOLDER}/build/outputs/apk/debug/app-debug.apk -m "${RELEASE_TITLE_HEAD} v${VERSION_NAME} ${RELEASE_TITLE_FOOT}" ${VERSION_CODE}
