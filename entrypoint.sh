1b4ba4ccd03fb34536273d7c30fa26665b9d1b5a
Taylor Allen <28820818+allenbyus@users.noreply.github.com>
2019-10-31 08:12:32 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 hub checkout master
-SEEK=`date '%Y%m%d%H%M%S'`
-VERSION=`echo "obase=64;${SEEK}"|bc`
+SEEK=`date '+%Y%m%d%H%M%S'`
+VERSION=`echo "obase=16;${SEEK}"|bc`
 hub release create -a ./${APP_FOLDER}/build/outputs/apk/debug/app-debug.apk -m "${RELEASE_TITLE}${VERSION}" $(date +%Y%m%d%H%M%S) 
